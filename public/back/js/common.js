c48822bc4e934f6629da20782e3a12ceb68d08a8
Your Name <zh.com>
2017-10-30 15:51:11 +0800

    公共侧边栏和头部完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -22,4 +22,33 @@ $(function () {
         }, 500);
 
     });
-})
+
+    //点击分类管理显示或者隐藏二级分类
+    //prev()上一层兄弟
+    // slideToggle()显示和隐藏之间切换
+    $(".child").prev().on("click", function () {
+        $(".child").slideToggle();
+    })
+    //点击icon_menu显示或者隐藏侧边栏 header的长度变长
+    $(".icon_menu").on("click", function () {
+        $(".ad_aside").toggleClass("now");
+        $(".ad_main").toggleClass("now");
+    })
+    //公用退出的功能
+    $(".icon_loginout").on("click",function () {
+        $('#logoutModal').modal("show");
+    });
+    $(".btn_logout").on("click",function () {
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            success:function (data) {
+                // console.log(data);
+                if(data.success){
+                    location.href="login.html";
+                }
+            }
+        })
+    })
+});
+
